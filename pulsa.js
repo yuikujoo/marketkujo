ebd3e82a957b37878290a502ca8ed0ad3450bfe7
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:23:37 +0700

    Create pulsa.js

---
+++ pulsa.js
@@ -0,0 +1,8 @@
+const pulsaData = [
+    { name: "Telkomsel", img: "telkomsel.jpg", link: "telkomsel.html", badge: "SALE", category: "pulsa" },
+    { name: "Indosat", img: "indosat.jpg", link: "indosat.html", badge: "PROMO", category: "pulsa" },
+    { name: "XL Axiata", img: "xl.jpg", link: "xl.html", badge: "FAST", category: "pulsa" },
+    { name: "Tri (3)", img: "tri.jpg", link: "tri.html", badge: "CHEAP", category: "pulsa" },
+    { name: "Smartfren", img: "smartfren.jpg", link: "smartfren.html", badge: "STABLE", category: "pulsa" },
+    { name: "Axis", img: "axis.jpg", link: "axis.html", badge: "PROMO", category: "pulsa" }
+];
