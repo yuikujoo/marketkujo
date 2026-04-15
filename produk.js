07d0f23b92c50166bbd7f622d02f20346fdd6dc7
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:15:22 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,31 +1,35 @@
-// Simpan file ini dengan nama: produk.js
+// EDIT DATA DAGANGAN DI SINI
 const dataProduk = [
   {
-    nama: "Moskov epic",
+    nama: "Moskov Allstar Sold", // Ada kata 'Sold', otomatis ke bawah & abu-abu
     harga: "Rp70.000",
     coret: "Rp100.000",
+    link: "moskov.html",
     badge: "PROMO", 
     foto: "https://i.supaimg.com/23b223c0-a5cc-4eb9-a50a-df79dbf49f6a/34a1789c-93a3-4a28-b972-ae8607989e27.png"
   },
   {
-    nama: "sold",
+    nama: "Weekly Pass",
     harga: "Rp26.500",
     coret: "Rp30.000",
+    link: "weekly.html",
     badge: "LIMITED",
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=Weekly+Pass"
   },
   {
-    nama: "sold",
+    nama: "86 Diamonds",
     harga: "Rp19.800",
     coret: "Rp23.000",
+    link: "dm.html",
     badge: "BEST PRICE",
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=86+DM"
   },
   {
-    nama: "sold",
+    nama: "Starlight Member",
     harga: "Rp62.000",
     coret: "Rp80.000",
-    badge: "best price", 
+    link: "starlight.html",
+    badge: "Sold Out", // Kata 'Sold' di sini juga memicu sistem otomatis
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=Starlight"
   }
 ];
