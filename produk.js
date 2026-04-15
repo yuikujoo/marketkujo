cd3ef0dfa86503854db2134249d060120b2c9338
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:00:29 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,31 +1,31 @@
 // Simpan file ini dengan nama: produk.js
 const dataProduk = [
   {
-    nama: "Moskov Allstar Sold",
+    nama: "sold",
     harga: "Rp70.000",
     coret: "Rp100.000",
     badge: "PROMO", 
     foto: "https://i.supaimg.com/23b223c0-a5cc-4eb9-a50a-df79dbf49f6a/34a1789c-93a3-4a28-b972-ae8607989e27.png"
   },
   {
-    nama: "Weekly Pass",
+    nama: "sold",
     harga: "Rp26.500",
     coret: "Rp30.000",
     badge: "LIMITED",
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=Weekly+Pass"
   },
   {
-    nama: "86 Diamonds",
+    nama: "sold",
     harga: "Rp19.800",
     coret: "Rp23.000",
     badge: "BEST PRICE",
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=86+DM"
   },
   {
-    nama: "Starlight Member Sold Out",
+    nama: "sold",
     harga: "Rp62.000",
     coret: "Rp80.000",
-    badge: "Sold Out", 
+    badge: "best price", 
     foto: "https://via.placeholder.com/300x400/111/bf953f?text=Starlight"
   }
 ];
