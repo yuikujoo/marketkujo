ae9ea910e5336bba54c181cf3fa92be37777604e
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:41:49 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,7 +1,7 @@
 // EDIT DATA DAGANGAN DI SINI
 const dataProduk = [
   {
-    nama: "Moskov Allstar Sold", // Ada kata 'Sold', otomatis ke bawah & abu-abu
+    nama: "Moskov Allstar"
     harga: "Rp70.000",
     coret: "Rp100.000",
     link: "moskov.html",
