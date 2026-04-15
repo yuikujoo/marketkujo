495013b2a2d43d3b811ff6e9486797fec5aa1437
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:23:55 +0700

    Create ewallet.js

---
+++ ewallet.js
@@ -0,0 +1,7 @@
+const ewalletData = [
+    { name: "GOPAY", img: "dana.jpg", link: "gopay.html", badge: "BEST", category: "ewallet" },
+    { name: "OVO", img: "ovo.jpg", link: "ovo.html", badge: "HOT", category: "ewallet" },
+    { name: "DANA", img: "gopay.jpg", link: "dana.html", badge: "PROMO", category: "ewallet" },
+    { name: "LinkAja", img: "linkaja.jpg", link: "linkaja.html", badge: "NEW", category: "ewallet" },
+    { name: "ShopeePay", img: "shopeepay.jpg", link: "shopeepay.html", badge: "HOT", category: "ewallet" }
+];
