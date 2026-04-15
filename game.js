67f7ea828406b2337adee1ef082da686ed94cbd6
yuikujoo <yuikujoo@gmail.com>
2026-04-15 15:23:05 +0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,5 @@
+const gameData = [
+    { name: "Mobile Legends", img: "ml.jpg", link: "ml.html", badge: "-10%", category: "game" },
+    { name: "Free Fire", img: "ff.jpg", link: "ff.html", badge: "HOT", category: "game" },
+    { name: "PUBG Mobile", img: "pubg.jpg", link: "pubg.html", badge: "NEW", category: "game" }
+];
